121253df26682820aab11ef2e86d8190e66593c1
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2025-09-19 01:29:49 +0200

    Remove Kw prefix from keywords

--- pptoken.match.generated.cpp
+++ pptoken.match.generated.cpp
@@ -1,69 +1,69 @@
 if(match[1].matched) { // NewLine
   token.kind = PPToken::kNewLine;
-} else if(match[2].matched) { // KwAuto
-  token.kind = PPToken::kKwAuto;
-} else if(match[3].matched) { // KwBreak
-  token.kind = PPToken::kKwBreak;
-} else if(match[4].matched) { // KwCase
-  token.kind = PPToken::kKwCase;
-} else if(match[5].matched) { // KwChar
-  token.kind = PPToken::kKwChar;
-} else if(match[6].matched) { // KwConst
-  token.kind = PPToken::kKwConst;
-} else if(match[7].matched) { // KwContinue
-  token.kind = PPToken::kKwContinue;
-} else if(match[8].matched) { // KwDefault
-  token.kind = PPToken::kKwDefault;
-} else if(match[9].matched) { // KwDo
-  token.kind = PPToken::kKwDo;
-} else if(match[10].matched) { // KwDouble
-  token.kind = PPToken::kKwDouble;
-} else if(match[11].matched) { // KwElse
-  token.kind = PPToken::kKwElse;
-} else if(match[12].matched) { // KwEnum
-  token.kind = PPToken::kKwEnum;
-} else if(match[13].matched) { // KwExtern
-  token.kind = PPToken::kKwExtern;
-} else if(match[14].matched) { // KwFloat
-  token.kind = PPToken::kKwFloat;
-} else if(match[15].matched) { // KwFor
-  token.kind = PPToken::kKwFor;
-} else if(match[16].matched) { // KwGoto
-  token.kind = PPToken::kKwGoto;
-} else if(match[17].matched) { // KwIf
-  token.kind = PPToken::kKwIf;
-} else if(match[18].matched) { // KwInt
-  token.kind = PPToken::kKwInt;
-} else if(match[19].matched) { // KwLong
-  token.kind = PPToken::kKwLong;
-} else if(match[20].matched) { // KwRegister
-  token.kind = PPToken::kKwRegister;
-} else if(match[21].matched) { // KwReturn
-  token.kind = PPToken::kKwReturn;
-} else if(match[22].matched) { // KwShort
-  token.kind = PPToken::kKwShort;
-} else if(match[23].matched) { // KwSigned
-  token.kind = PPToken::kKwSigned;
-} else if(match[24].matched) { // KwSizeof
-  token.kind = PPToken::kKwSizeof;
-} else if(match[25].matched) { // KwStatic
-  token.kind = PPToken::kKwStatic;
-} else if(match[26].matched) { // KwStruct
-  token.kind = PPToken::kKwStruct;
-} else if(match[27].matched) { // KwSwitch
-  token.kind = PPToken::kKwSwitch;
-} else if(match[28].matched) { // KwTypedef
-  token.kind = PPToken::kKwTypedef;
-} else if(match[29].matched) { // KwUnion
-  token.kind = PPToken::kKwUnion;
-} else if(match[30].matched) { // KwUnsigned
-  token.kind = PPToken::kKwUnsigned;
-} else if(match[31].matched) { // KwVoid
-  token.kind = PPToken::kKwVoid;
-} else if(match[32].matched) { // KwVolatile
-  token.kind = PPToken::kKwVolatile;
-} else if(match[33].matched) { // KwWhile
-  token.kind = PPToken::kKwWhile;
+} else if(match[2].matched) { // Auto
+  token.kind = PPToken::kAuto;
+} else if(match[3].matched) { // Break
+  token.kind = PPToken::kBreak;
+} else if(match[4].matched) { // Case
+  token.kind = PPToken::kCase;
+} else if(match[5].matched) { // Char
+  token.kind = PPToken::kChar;
+} else if(match[6].matched) { // Const
+  token.kind = PPToken::kConst;
+} else if(match[7].matched) { // Continue
+  token.kind = PPToken::kContinue;
+} else if(match[8].matched) { // Default
+  token.kind = PPToken::kDefault;
+} else if(match[9].matched) { // Do
+  token.kind = PPToken::kDo;
+} else if(match[10].matched) { // Double
+  token.kind = PPToken::kDouble;
+} else if(match[11].matched) { // Else
+  token.kind = PPToken::kElse;
+} else if(match[12].matched) { // Enum
+  token.kind = PPToken::kEnum;
+} else if(match[13].matched) { // Extern
+  token.kind = PPToken::kExtern;
+} else if(match[14].matched) { // Float
+  token.kind = PPToken::kFloat;
+} else if(match[15].matched) { // For
+  token.kind = PPToken::kFor;
+} else if(match[16].matched) { // Goto
+  token.kind = PPToken::kGoto;
+} else if(match[17].matched) { // If
+  token.kind = PPToken::kIf;
+} else if(match[18].matched) { // Int
+  token.kind = PPToken::kInt;
+} else if(match[19].matched) { // Long
+  token.kind = PPToken::kLong;
+} else if(match[20].matched) { // Register
+  token.kind = PPToken::kRegister;
+} else if(match[21].matched) { // Return
+  token.kind = PPToken::kReturn;
+} else if(match[22].matched) { // Short
+  token.kind = PPToken::kShort;
+} else if(match[23].matched) { // Signed
+  token.kind = PPToken::kSigned;
+} else if(match[24].matched) { // Sizeof
+  token.kind = PPToken::kSizeof;
+} else if(match[25].matched) { // Static
+  token.kind = PPToken::kStatic;
+} else if(match[26].matched) { // Struct
+  token.kind = PPToken::kStruct;
+} else if(match[27].matched) { // Switch
+  token.kind = PPToken::kSwitch;
+} else if(match[28].matched) { // Typedef
+  token.kind = PPToken::kTypedef;
+} else if(match[29].matched) { // Union
+  token.kind = PPToken::kUnion;
+} else if(match[30].matched) { // Unsigned
+  token.kind = PPToken::kUnsigned;
+} else if(match[31].matched) { // Void
+  token.kind = PPToken::kVoid;
+} else if(match[32].matched) { // Volatile
+  token.kind = PPToken::kVolatile;
+} else if(match[33].matched) { // While
+  token.kind = PPToken::kWhile;
 } else if(match[34].matched) { // CharacterConstant
   const auto &it = match.str(0);
   { if (!it.ends_with("'")) throw std::runtime_error("Unclosed character constant"); }
